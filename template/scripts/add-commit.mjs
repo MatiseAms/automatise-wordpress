b29336bfe51dde64912883c59637b91e56a27976
Stephan Hoogland <stephan@shoogland.com>
2020-03-17 11:41:47 +0100

    fix path commit.mjs

--- template/scripts/add-commit.mjs
+++ template/scripts/add-commit.mjs
@@ -1,20 +1,20 @@
-import { readFile, writeFile } from 'fs'
+import { readFile, writeFile } from 'fs';
 
-const someFile = './themesrc/functions.php'
+const someFile = './src/functions.php';
 
 readFile(someFile, 'utf8', function(err, data) {
 	if (err) {
 		// eslint-disable-next-line no-console
-		return console.log(err)
+		return console.log(err);
 	}
 
-	let result = data.replace(/<commit>/g, process.env.GITHUB_SHA.substr(0,8))
+	let result = data.replace(/<commit>/g, process.env.GITHUB_SHA.substr(0,8));
 	if(process.env.GITHUB_REF){
-		result = result.replace(/<branch_ref>/g, process.env.GITHUB_REF)
+		result = result.replace(/<branch_ref>/g, process.env.GITHUB_REF);
 	}
 
 	writeFile(someFile, result, 'utf8', function(err) {
 		// eslint-disable-next-line no-console
-		if (err) return console.log(err)
-	})
-})
+		if (err) return console.log(err);
+	});
+});
